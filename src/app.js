06f31e257ffbc4f4ce8712c00fc502f4c8e818d1
Anderson <anderson.fagundes4@gmail.com>
2026-04-05 16:49:33 +0100

    ✨ feat(server): add CORS, JSON parsing, and APOD routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import apodRoutes from './routes/apodRoutes.js';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_PREFIX = '/api/v1';
+
+//api_routes
+app.use(API_PREFIX, apodRoutes); //Astronomy Picture of the Day
 
 export default app;
