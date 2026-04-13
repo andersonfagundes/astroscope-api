833010299418865ff827d4eda6d44ce61f386367
Anderson <anderson.fagundes4@gmail.com>
2026-04-13 08:34:15 +0100

    ♻️ refactor(apod): remove controller, routes, and service

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import apodRoutes from './apodRoutes.js';
 import epicRoutes from './epicRoutes.js';
 
 const router = express.Router();
 
 //api routes
-router.use('/apod', apodRoutes);
 router.use('/epic', epicRoutes);
 
 export default router;
